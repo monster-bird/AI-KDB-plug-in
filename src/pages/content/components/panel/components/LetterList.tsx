190a1a4e9f27855eb60e382b9d2963552179c2e3
zhangyang <18111002318@163.com>
2023-07-18 18:08:14 +0800

    修复了一些潜在问题

--- src/pages/content/components/panel/components/LetterList.tsx
+++ src/pages/content/components/panel/components/LetterList.tsx
@@ -32,13 +32,12 @@ export default function LetterList() {
       setOriginList([...global.letterList])
       setLetterList([...global.letterList])
 
-
-
+      setSearchTerm(global.searchWords)
+      setRealTime(global.realMode)
       setLoading(false)
-      console.log(global.currentTime);
-
+      findSelectKeyList(global.searchWords, global.letterList)
       refleshTime(global.currentTime)
-
+      setNowSelectKey(global.currentSelectKey)
     }
 
   }, [])
@@ -46,58 +45,76 @@ export default function LetterList() {
   useEffect(() => {
     if (originList.length > 0) {
       refleshTime(global.currentTime)
-
+      
     }
   }, [global.currentTime, originList])
   const refleshTime = (_currentTime: number) => {
     let maxIndex = -1
     originList.map((item, index) => {
-      if (item.from - 0.7 <= _currentTime) {
+      if (Math.floor(item.from) <= _currentTime) {
         maxIndex = index
       }
 
     });
-    console.log(maxIndex);
+
 
     setCurrentIndex(maxIndex);
 
   }
   const getLetterData = () => {
-    axiosInstance.get(`/v2/ai-notes/${summary.currentBvid}/subtitle`).then(value => {
+    const queryString = window.location.search;
+
+    const urlParams = new URLSearchParams(queryString);
+
+
+    let _p = urlParams.get('p');
+    if (!_p) {
+      _p = '';
+    } else {
+      _p = '%3Fp=' + _p;
+    }
+    axiosInstance.get(`/v2/ai-notes/${summary.currentBvid+_p}/subtitle`).then(value => {
       setLetterList(value)
       setLoading(false)
       global.setLetterList(value)
       setOriginList([...value])
-      console.log('字幕加载完毕');
     })
   }
+  const findSelectKeyList = (value,letterList) => {
+    setKeyList([])
+    let tempList = []
+    
+    letterList.map((item, i) => {
+      let escapedValue = value.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+      let pattern = new RegExp(`(${escapedValue})`, "gi");
+      const contentList = item.content.split(pattern)
 
+      contentList.map((_, j) => {
+        if (pattern.test(_))
+          tempList.push(i + '-' + j)
+      })
+    })
+    setKeyList(tempList)
+    
+    setNowSelectKey(tempList[0])
+    global.setcurrentSelectKey(tempList[0])
+  }
   const handleInputChange = (event: { target: { value: any; }; }) => {
     const value = event.target.value;
     setSearchTerm(value);
+    global.setSearchWords(value)
     // 调用防抖函数
     // setLetterList(searchArray([...originList], value))
-    const searchList = searchTerm.split(' ')
     if (value === '' || !value) {
       setKeyList([])
 
       return
     }
+    console.log('search');
+    
     setRealTime(false)
-    setKeyList([])
-    let tempList = []
-    letterList.map((item, i) => {
-      // const mapValue = content.replace(new RegExp(searchTerm, "gi"), '%');
-      const mapValue = item.content.replace(new RegExp(value, "gi"), '%');
-      const contentList = mapValue.split('%')
-
-      contentList.map((_, j) => {
-        if (j !== contentList.length - 1)
-          tempList.push(i + '-' + j)
-      })
-    })
-    setKeyList(tempList)
-    setNowSelectKey(tempList[0])
+    global.setRealMode(false)
+    findSelectKeyList(value, letterList)
   };
 
   // const searchArray = (array, searchTerm) => {
@@ -114,6 +131,7 @@ export default function LetterList() {
   const onCheckBoxChange = (e) => {
 
     setRealTime(e.target.checked)
+    global.setRealMode(e.target.checked)
   }
 
   const scrollRef = React.useRef(null);
@@ -129,7 +147,6 @@ export default function LetterList() {
 
       }
       const offset = currentLine.offsetTop - 460;
-      console.log(offset);
 
       if (currentIndex > 10) {
         scrollContainer.scrollTo({
@@ -156,7 +173,7 @@ export default function LetterList() {
   }, [nowSelectKey])
   const handleKeyUp = () => {
     setRealTime(false)
-
+    global.setRealMode(false)
     let index = keyList.findIndex((value) => value === nowSelectKey)
 
     if (index === -1) {
@@ -169,30 +186,35 @@ export default function LetterList() {
     }
 
     setNowSelectKey(keyList[index])
+    global.setcurrentSelectKey(keyList[index])
+
   }
   const handleKeyDown = () => {
     setRealTime(false)
-    let index = keyList.findIndex((value) => value === nowSelectKey)
+    global.setRealMode(false)
 
+    let index = keyList.findIndex((value) => value === nowSelectKey)
+    
     if (index === -1) {
       return
     }
     index = (index + 1) % keyList.length
 
     setNowSelectKey(keyList[index])
-
+    global.setcurrentSelectKey(keyList[index])
   }
+
   const renderLineRegs = (content, _index) => {
     if (!searchTerm) {
       return content; // 当 searchTerm 为空时，直接返回 false
     }
-    const searchList = searchTerm.split(' ')
 
-    const mapValue = content.replace(new RegExp(searchTerm, "gi"), '%');
-    const contentList = mapValue.split('%')
+    // const mapValue = content.replace(new RegExp(searchTerm, "gi"), '*%#');
+    let escapedValue = content.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+    let pattern = new RegExp(`(${escapedValue})`, "gi");
+    const contentList = content.split(pattern)
 
-
-    if (contentList.length > 1) {
+    if (contentList.length >= 0) {
 
       return (
         <>
@@ -200,12 +222,17 @@ export default function LetterList() {
           {
             contentList.map((item, index) => {
               return <>
+              {
+                pattern.test(item)?
+                <span className={`${nowSelectKey === (_index + '-' + index) ? 'selectKey ' : ' '}` +
+                tw`${nowSelectKey === (_index + '-' + index) ? ' bg-[#fb9434] ' : ' bg-[#fcfa04]'}`}>
+                  {item}
+                </span>
+                :
                 <span>{item}</span>
-                {
-                  index != contentList.length - 1 ? <span
-                    className={`${nowSelectKey === (_index + '-' + index) ? 'selectKey ' : ' '}` +
-                      tw`${nowSelectKey === (_index + '-' + index) ? ' bg-[#fb9434] ' : ' bg-[#fcfa04]'}`}>{searchList[0]}</span> : ''
-                }
+              }
+
+
               </>
 
             })
@@ -228,7 +255,7 @@ export default function LetterList() {
             return (
               <span className={tw`${index === currentIndex ? ' text-red-500 highlight' : ''}`}>
                 <Tooltip title={secondToTimeStr(item.from)}>
-                  <span className={tw`cursor-pointer hover:underline`}
+                  <span className={tw`cursor-pointer hover:underline` + `dm-info-dm`}
                     onClick={event => {
                       if (typeof item.from === 'number') {
                         window.postMessage({
@@ -265,28 +292,31 @@ export default function LetterList() {
           <div className={tw`h-96 overflow-y-scroll `} ref={scrollRef}>
             {
               letterList.map((item, index) => (
-                <div key={index + item.from} className={tw`flex pl-3 pr-3 cursor-pointer  hover:bg-gray-200 
+                <div key={index + item.from} className={tw`flex items-center pb-1 pt-1 pl-3 pr-3 cursor-pointer   hover:bg-gray-200 
               ${index === currentIndex ? 'text-red-400 highlight' : ''}`
                 }
                   onClick={event => {
                     if (typeof item.from === 'number') {
                       window.postMessage({
                         type: 'change-video-playback-time',
-                        data: item.from
+                        data: Math.floor(item.from)
                       });
+                      refleshTime(Math.floor(item.from))
+                      
+                      
                     }
 
                     event.stopPropagation();
                   }}
                 >
-                  <div className={tw`w-12 relative`}>
+                  <div className={tw`w-12 relative ` + `dm-info-time`}>
                     {currentIndex === index ? <span className={tw` absolute -left-3 `}>•</span> : ''}
 
                     {secondToTimeStr(item.from)}
                   </div>
 
 
-                  <div >
+                  <div className={tw`dm-info-dm w-4/5`}>
 
                     {
                       renderLineRegs(item.content, index)
@@ -317,7 +347,7 @@ export default function LetterList() {
   return (
     <div className={tw`pl-3 pr-3 mt-3`}>
       <div className={tw`flex justify-between items-center`}>
-        <Input placeholder="搜索字幕" onChange={handleInputChange} />
+        <Input placeholder="搜索字幕" onChange={handleInputChange} value={searchTerm}/>
         <div className={tw`flex justify-center items-center ml-2`}>
           <span className={tw`relative`}>
             {
@@ -345,7 +375,7 @@ export default function LetterList() {
       <div className={tw`flex items-center justify-between `}>
         {
           letterList.length > 0 ?
-            <div className={tw`mt-2`}><p>共{letterList.length + 1}条字幕{keyList.length > 0 ? <>，搜索到{keyList.length}条</> : ''}</p></div> : ''
+            <div className={tw`mt-2 text-[14px]`}><p>共{letterList.length + 1}条字幕{keyList.length > 0 ? <>，搜索到{keyList.length}条</> : ''}</p></div> : ''
         }
         <Checkbox onChange={onCheckBoxChange} checked={realTime}>实时滚动</Checkbox>
       </div>
